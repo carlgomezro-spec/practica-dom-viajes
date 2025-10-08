568bf23e12faec248764d8e928cacbd3fd0e6ec9
CARLOS <carl.gomezro@gmail.com>
2025-10-08 15:44:21 +0200

    resolved

--- js/script.js
+++ js/script.js
@@ -16,16 +16,21 @@ const viajes = [
   }
 ];
 
-const result = document.getElementById("result")
+const result = document.getElementById("viajes-container")
 
 for (let viaje of viajes) {
 result.innerHTML += `
+  <li class="card">
   <img src=${viaje.url_img} alt=${viaje.url_img}/>
   <h2>${viaje.title}</h2>
   <p>${viaje.description}</p>
+  </li>
   `}
 
 result.style.fontSize = "10px"
+result.style.display = "flex"
+result.style.gap = "10px"
+result.style.alignItems = "left"
 
 
 
@@ -42,3 +47,21 @@ const cities = [
 "Zaragoza"
 ];
 
+const container = document.getElementById('destinos-container');
+
+const select = document.createElement('select');
+select.style.width = '100%';
+select.style.padding = '10px';
+select.style.fontSize = '16px';
+select.style.border = '1px solid #ccc';
+select.style.borderRadius = '4px';
+select.style.boxSizing = 'border-box';
+
+  cities.forEach(city => {
+  const option = document.createElement('option');
+  option.value = city;
+  option.textContent = city;
+  select.appendChild(option);
+});
+
+container.appendChild(select);
